e84b93f7368c676b339e07ce58a5384c3656bf9f
Lewis Rotchell <rotchell92@gmail.com>
2021-11-06 19:25:46 +0000

    Finished desktop primary header and added util classes

--- layout/Header.tsx
+++ layout/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 interface HeaderProps {}
 
@@ -20,13 +21,26 @@ const navLinks = [
 
 export const Header: React.FC<HeaderProps> = ({}) => {
   return (
-    <header>
-      <Image
-        src="/images/shared/desktop/logo.svg"
-        height={26}
-        width={236}
-        alt={"coffee roasters logo"}
-      ></Image>
+    <header className="primary-header flex">
+      <div>
+        <Image
+          src="/images/shared/desktop/logo.svg"
+          height={26}
+          width={236}
+          alt={"coffee roasters logo"}
+        ></Image>
+      </div>
+      <nav>
+        <ul className="flex">
+          {navLinks.map((item, index) => (
+            <li key={index}>
+              <Link href={item.link}>
+                <a className="uppercase fs-300">{item.name}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 };
---
+++ layout/Layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Header } from "./Header";
+
+interface LayoutProps {}
+
+export const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <>{children}</>
+    </>
+  );
+};
